b927a4258aa721329e58841fe3f643952b4fe4c4
Akhona Trom <akhonatrom1@gmail.com>
2025-06-15 17:41:04 +0200

    add changes

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,9 +3,10 @@ from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
+        """Create a calculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, -2), -3)
         self.assertEqual(self.calc.add(-5, 5), 0)
@@ -27,9 +28,9 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
         self.assertEqual(self.calc.divide(-9, 3), -3)
-        self.assertIsNone(self.calc.divide(10, 0))  
-        self.assertIsNone(self.calc.divide(0, 0))   
-        self.assertEqual(self.calc.divide(0, 5), 0) 
+        self.assertIsNone(self.calc.divide(10, 0))  # Division by zero
+        self.assertIsNone(self.calc.divide(0, 0))   # Still invalid
+        self.assertEqual(self.calc.divide(0, 5), 0)
 
 if __name__ == '__main__':
     unittest.main()
